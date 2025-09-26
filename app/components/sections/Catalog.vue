28be4f050cceab9cb9badb9666c3b223ad0b4613
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-09-26 15:01:02 +0700

    REFACT: move catalog section to section components

---
+++ app/components/sections/Catalog.vue
@@ -0,0 +1,35 @@
+<template>
+  <section id="katalog">
+    <div class="main-container p-20">
+      <h2 class="h1 mt-0 text-center mb-12">Katalog Produk Batu Alam</h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div
+          v-for="(item, index) in catalogs"
+          :key="index"
+          class="aspect-square w-full overflow-hidden"
+          @click="onClickImage(item)">
+          <img
+            :src="item"
+            :alt="`batu alam ${index}`"
+            width="500"
+            height="500"
+            class="object-cover object-center w-full h-full transition-default hover:scale-105" />
+        </div>
+      </div>
+    </div>
+    <PreviewImage v-model="isOpenPreview" :selected-image="selectedImage" @close-modal="isOpenPreview = false" />
+  </section>
+</template>
+
+<script lang="ts" setup>
+const selectedImage = ref<string>( "" )
+const isOpenPreview = ref<boolean>( false )
+
+const onClickImage = ( val: string ) => {
+  isOpenPreview.value = true
+  selectedImage.value = val
+}
+const catalogs = Array.from( { length: 47 }, ( _, i ) =>
+  `/images/batu-alam/batu-alam-${i + 1}.webp`,
+)
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -85,26 +85,7 @@
         </div>
       </div>
     </section>
-    <section id="katalog">
-      <div class="main-container p-20">
-        <h2 class="h1 mt-0 text-center mb-12">Katalog Produk Batu Alam</h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div
-            v-for="(item, index) in catalogs"
-            :key="index"
-            class="aspect-square w-full overflow-hidden"
-            @click="onClickImage(item)">
-            <img
-              :src="item"
-              :alt="`batu alam ${index}`"
-              width="500"
-              height="500"
-              class="object-cover object-center w-full h-full transition-default hover:scale-105" />
-          </div>
-        </div>
-      </div>
-      <PreviewImage v-model="isOpenPreview" :selected-image="selectedImage" @close-modal="isOpenPreview = false" />
-    </section>
+    <SectionsCatalog/>
     <section class="bg-gray-100">
       <div class="main-container p-20">
         <h2 class="h1 mt-0 text-center mb-12">Layanan Pemasangan</h2>
@@ -136,7 +117,6 @@
           </div>
         </div>
       </div>
-      <PreviewImage v-model="isOpenPreview" :selected-image="selectedImage" @close-modal="isOpenPreview = false" />
     </section>
     <section>
       <div class="main-container p-20">
@@ -172,16 +152,3 @@
     </section>
   </div>
 </template>
-
-<script lang="ts" setup>
-const selectedImage = ref<string>( "" )
-const isOpenPreview = ref<boolean>( false )
-
-const onClickImage = ( val: string ) => {
-  isOpenPreview.value = true
-  selectedImage.value = val
-}
-const catalogs = Array.from( { length: 47 }, ( _, i ) =>
-  `/images/batu-alam/batu-alam-${i + 1}.webp`,
-)
-</script>
