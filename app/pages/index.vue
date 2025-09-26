0bff9f40f4da4ca42bb8324024998db722450d29
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-09-26 15:29:51 +0700

    FEAT: testimoni section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -85,7 +85,7 @@
         </div>
       </div>
     </section>
-    <SectionsCatalog/>
+    <SectionsCatalog />
     <section class="bg-gray-100">
       <div class="main-container p-20">
         <h2 class="h1 mt-0 text-center mb-12">Layanan Pemasangan</h2>
@@ -146,6 +146,28 @@
                 loading="eager"
                 class="object-contain w-full h-full" />
             </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-gray-100">
+      <div class="main-container p-20">
+        <h2 class="h1 mt-0 text-center mb-12">Testimoni Pelanggan</h2>
+        <div
+          class="bg-gradient-to-b from-gray-100 to-white hover:shadow-2xl transition-default p-8 text-center rounded-xl relative">
+          <blockquote cite="https://www.example.com/artikel" class="text-2xl text-center mb-8 max-w-2xl mx-auto">
+            <Icon name="fa7-solid:quote-left" />
+            Hasil pemasangan sangat rapi, rumah jadi terlihat lebih alami dan elegan.
+            <Icon name="fa7-solid:quote-right" />
+          </blockquote>
+          <div class="flex items-center gap-2 text-yellow-300 justify-center mb-8">
+            <template v-for="item in 5" :key="item">
+              <Icon name="fa7-solid:star" />
+            </template>
+          </div>
+          <div
+            class="overflow-hidden size-20 aspect-square rounded-full absolute bottom-0 inset-x-0 mx-auto translate-y-1/2 border-4 border-white">
+            <img src="@/assets/images/ianfebi.webp" alt="calendar" class="w-full h-full object-contain object-center" />
           </div>
         </div>
       </div>
